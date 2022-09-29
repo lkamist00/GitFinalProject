4f24d181c39f998d6e94208543494b010c9ddad4
lkamist00 <lkamist00@gmail.com>
2022-09-28 20:06:43 -0400

    first commit

---
+++ src/finalProject/finalClass.java
@@ -0,0 +1,13 @@
+package finalProject;
+
+public class finalClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is final project that we use to push code to Github");
+		
+		
+
+	}
+
+}
